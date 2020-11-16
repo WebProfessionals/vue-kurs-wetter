67232fe244369c0cbd97f2dde3ed819f4dd14a03
Rolf Eggenberger <rolf@kunstwort.com>
2020-11-16 18:48:22 +0100

    feat: api connection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,52 @@
 <template>
   <div class="home">
 
-    <input>
+    <input v-model="city">
+    <button @click="fetchWeather">Suche</button>
     <br>
-    {{forecast}}
+    <p
+      v-if="forecast.main">
+      {{forecastTemperature}}
+    </p>
+
 
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
 
-export default {
-  name: 'Home',
-  data () {
-    return {
-      city: '',
-      forecast: '20 Grad, Sonne'
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        city: '',
+        forecast: {}
+      }
+    },
+    computed: {
+      forecastTemperature () {
+        return this.forecast.main.temp + ' Grad Celcius'
+      },
+      apiUrl () {
+        return 'https://api.openweathermap.org/data/2.5/weather?q=' + this.city + '&units=metric&lang=de&appid=67250520d949de08b0b5023929e306b9'
+      }
+    },
+    methods: {
+      fetchWeather() {
+        let self = this
+        console.log('Wetter wird ausgegeben')
+        axios.get(this.apiUrl)
+                .then(function (response) {
+                  // handle success
+                  self.forecast = response.data;
+                })
+                .catch(function (error) {
+                  // handle error
+                  console.log(error);
+                })
+      }
     }
-  },
-  methods: {
-    fetchWeather() {
 
-    }
   }
-
-}
 </script>
